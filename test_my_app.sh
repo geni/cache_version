6293909c8254dbf7654e2dd9f47fd2e40cb38c9d
Scott Steadman <scott.steadman@geni.com>
2026-04-09 10:06:09 -0700

    add test_my_app.sh
     Jenkins will call this.

---
+++ test_my_app.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+bundle config --local clean true
+bundle config --local path vendor/bundle
+bundle config --local without vscode
+
+# The bundler version can change between branches
+rm -f Gemfile.lock
+
+bundle install
+
+bundle exec rails test
